9bf8654e7ea0a6046d83932a05e20a9395bece44
faisal-saddique <faisalsaddique7964@gmail.com>
2023-08-14 15:18:45 +0500

    added reqs and updated k

--- pages/Create_KnowledgeBase.py
+++ pages/Create_KnowledgeBase.py
@@ -24,53 +24,57 @@ st.title("Upload Documents")
 accepted_file_types = ["pdf", "csv", "docx", "xlsx"]
 uploaded_files = st.file_uploader("Upload one or more files", accept_multiple_files=True, type=accepted_file_types)
 
-if uploaded_files:
-    docs = None
-    tot_len = 0
+try:
+    if uploaded_files:
+        docs = None
+        tot_len = 0
 
-    for file in uploaded_files:
-        file_extension = file.name.split(".")[-1].upper()
-        st.write(f'File: {file.name}, Extension: {file_extension}')
-        file_content = file.read()  # Read the content of the uploaded file
+        for file in uploaded_files:
+            file_extension = file.name.split(".")[-1].upper()
+            st.write(f'File: {file.name}, Extension: {file_extension}')
+            file_content = file.read()  # Read the content of the uploaded file
 
-        # st.write(file_content)
+            # st.write(file_content)
 
-        if file_extension == 'PDF':
-            if docs is None:
-                docs = parse_readable_pdf(file_content)
+            if file_extension == 'PDF':
+                if docs is None:
+                    docs = parse_readable_pdf(file_content)
+                else:
+                    docs = docs + parse_readable_pdf(file_content)
+            elif file_extension == 'DOCX':
+                if docs is None:
+                    docs = parse_docx(file_content)
+                else:
+                    docs = docs + parse_docx(file_content)
+
+            elif file_extension == 'XLSX':
+                if docs is None:
+                    docs = parse_xlsx(file_content)
+                else:
+                    docs = docs + parse_xlsx(file_content)
+            
+            elif file_extension == 'CSV':
+                if docs is None:
+                    docs = parse_csv(file_content)
+                else:
+                    docs = docs + parse_csv(file_content)
             else:
-                docs = docs + parse_readable_pdf(file_content)
-        elif file_extension == 'DOCX':
-            if docs is None:
-                docs = parse_docx(file_content)
-            else:
-                docs = docs + parse_docx(file_content)
+                raise ValueError("File type not supported!")
+            
+            # for doc in docs:
+            #     st.success(doc)
 
-        elif file_extension == 'XLSX':
-            if docs is None:
-                docs = parse_xlsx(file_content)
-            else:
-                docs = docs + parse_xlsx(file_content)
-        
-        elif file_extension == 'CSV':
-            if docs is None:
-                docs = parse_csv(file_content)
-            else:
-                docs = docs + parse_csv(file_content)
-        else:
-            raise ValueError("File type not supported!")
-        
-        # for doc in docs:
-        #     st.success(doc)
+        chunked_docs = refined_docs(docs)
 
-    chunked_docs = refined_docs(docs)
+        # for doc in chunked_docs:
+        #     st.warning(doc)
 
-    # for doc in chunked_docs:
-    #     st.warning(doc)
+        no_of_tokens = num_tokens_from_string(chunked_docs)
+        st.write(f"Number of tokens: \n{no_of_tokens}")
 
-    no_of_tokens = num_tokens_from_string(chunked_docs)
-    st.write(f"Number of tokens: \n{no_of_tokens}")
+        with st.spinner("Creating Index..."):
+            st.session_state.index = add_vectors_to_FAISS(chunked_docs=chunked_docs)
+            st.success("Done! Please headover to chatbot to start interacting with your data.")
 
-    with st.spinner("Creating Index..."):
-        st.session_state.index = add_vectors_to_FAISS(chunked_docs=chunked_docs)
-        st.success("Done! Please headover to chatbot to start interacting with your data.")
+except Exception as e:
+    st.error("An error occured while indexing your documents: {e}\n\nPlease fix the error and try again.")
